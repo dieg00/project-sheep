9f9f918005bb25a314e6e8f6240fff9065dacf5a
dieg00 <diegogonvillar2@gmail.com>
2017-07-25 20:52:31 +0200

    Login Working
    User: Diego
    PW: dot

--- basic.js
+++ basic.js
@@ -1,11 +1,23 @@
 var dot;
 
-alert("hola0");
+//alert("hola0");
 //button2.onclick.prompt("Escriba el nombre de su perro");
-dot = prompt("Escriba el nombre de su perro");
-alert("Se llama " + dot);
+//dot = prompt("Escriba el nombre de su perro");
+//alert("Se llama " + dot);
 
 function boton(){
   var nombre = prompt("Escriba su nombre");
 return nombre;
+}
+
+function login(){
+  var  user = document.getElementById("user").value;
+  var password = document.getElementById("password").value;
+  if (user == "Diego" && password == "dot") {
+    window.location.href = "dashboard.html";
+  } else {
+    window.location.href = "login.html";
+  }
+
+
 }
